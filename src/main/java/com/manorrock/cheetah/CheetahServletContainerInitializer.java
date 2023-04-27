e82db66dce3ab03391fa2f6a9e3ff9e8ce9fdb8f
Manfred Riem <mriem@manorrock.com>
2023-04-27 08:14:12 -0500

    Fixes issue #1 - Create ServletContainerInitializer

---
+++ src/main/java/com/manorrock/cheetah/CheetahServletContainerInitializer.java
@@ -0,0 +1,25 @@
+package com.manorrock.cheetah;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import java.util.Set;
+
+/**
+ * The ServletContainerInitializer to enable Manorrock Cheetah.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class CheetahServletContainerInitializer implements ServletContainerInitializer {
+
+    /**
+     * On startup.
+     * 
+     * @param classes the classes.
+     * @param servletContext the Servlet context.
+     * @throws ServletException when a serious error occurs.
+     */
+    public void onStartup(Set<Class<?>> classes, ServletContext servletContext)
+            throws ServletException {
+    }
+}
